56b0bfad312a1906a0785cce1116d70ced374c5f
Andr3w <tacticalops@centrum.cz>
2020-03-19 09:54:32 +0100

    Update dayofranger.js
    Fix date to ISO

--- js/dayofranger.js
+++ js/dayofranger.js
@@ -575,7 +575,10 @@
     };
 
     DOR.prototype.parseToISODate = function(date) {
-        return date.toISOString().split('T')[0];
+        var year = date.getFullYear();
+        var month = (date.getMonth()+1) < 10 ? '0' + (date.getMonth()+1) : (date.getMonth()+1);
+        var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+        return (year + '-' + month + '-' + day);
     };
 
     var DORFactory = {
